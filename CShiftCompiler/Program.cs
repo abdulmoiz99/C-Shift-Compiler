d0dec976f92b46e445b195a6ed75f64b596b927c
Abdul Moiz <moiza0799@gmail.com>
2020-11-21 00:42:42 +0500

    Parsing String tokens: incomplete

--- CShiftCompiler/Program.cs
+++ CShiftCompiler/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,9 +16,58 @@ namespace CShiftCompiler
         //Application.EnableVisualStyles();
         //Application.SetCompatibleTextRenderingDefault(false);
         //Application.Run(new Form1());
+
+
         static void Main()
         {
-            Console.WriteLine("HELLO WORLD");
+            //tokens list
+            List<string> tokens = new List<string>();
+
+            //
+            char[] Punctuators = { ';', ',', ':', '.', '(', ')', '{', '}', '[', ']' };
+
+            var temp = string.Empty;
+
+            //Reading input files
+            var reader = new StreamReader(Application.StartupPath + @"\input.txt");
+            string testString = "\"asdasdasdasdasd\n";
+            var cr = testString.ToCharArray();
+            //var cr = reader.ReadToEnd().ToCharArray();
+
+            //Reading 
+            for (int i = 0; i < cr.Count(); i++)
+            {
+                if (cr[i] == '"')
+                {
+                    Temp.Add(cr[i]);
+                    i++;
+                    while (cr[i] != '"' )
+                    {
+
+                        i++;
+                        if (cr[i] == '\n' ||i < cr.Count() - 1)
+                        {
+                            tokens.Add(Temp.Empty());
+                            break;
+                        }
+                        Temp.Add(cr[i]);
+                    }
+                    tokens.Add(Temp.Empty());
+                }
+                else if (cr[i] == '\'')
+                {
+
+                }
+                else
+                {
+
+                }
+            }
+            foreach (var item in tokens)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
         }
     }
 }
---
+++ CShiftCompiler/Temp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CShiftCompiler
+{
+    
+    static class Temp
+    {
+        private  static string temp { get; set; }
+
+        static public void Add(char c)
+        {
+            temp += c;
+        }
+        static public string Empty()
+        {
+            string value = temp;
+            temp = string.Empty;
+           
+            return value;
+        }
+       
+    }
+}
