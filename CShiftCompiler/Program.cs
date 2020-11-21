b8f40cdca6738b94eebce8201ce848058204ae49
Abdul Moiz <moiza0799@gmail.com>
2020-11-21 16:15:16 +0500

    String Condions

--- CShiftCompiler/Program.cs
+++ CShiftCompiler/Program.cs
@@ -29,51 +29,114 @@ namespace CShiftCompiler
             var temp = string.Empty;
 
             //Reading input files
-            var reader = new StreamReader(Application.StartupPath + @"\input.txt");
-            //string testString = @"ksdksdlsdsdd";
+            var reader = new StreamReader(Application.StartupPath + @"\Input\input1.txt");
+            // string testString = "ksdk sdlsdsdd";
             //var cr = testString.ToCharArray();
             var cr = reader.ReadToEnd().ToCharArray();
 
             //Reading 
             for (int i = 0; i < cr.Count(); i++)
             {
+                //FOR STRING
+                // Start Conditions - "
                 if (cr[i] == '"')
                 {
-                    Temp.Add(cr[i]);
-                    i++;
-                    while (cr[i] != '"' && i < cr.Count() - 1)
+                    do // For appending first inverted comma
                     {
-                        if (cr[i] == '\\') //Single Slash ('\')
+                        // Include Conditions ("\") - increment for the next character
+                        if (cr[i] == '\\')
                         {
-                            Temp.Add(cr[i]);
-                            i++;
-                            Temp.Add(cr[i]);
+                            if (!string.IsNullOrEmpty(cr[i + 1].ToString()))
+                            {
+                                Temp.Add(cr[i++]);
+                            }
+                            else
+                            {
+                                tokens.Add(Temp.Empty());
+                                break;
+                            }
                         }
-                        else if (cr[i] == '\n')
-                        {
-                            tokens.Add(Temp.Empty());
-                            break;
-                        }
-                        else 
-                        {
-                            Temp.Add(cr[i]);
-                        }
-                        i++;
-                    }
-                    if (cr[i] == '"' || cr[i] == '\\') 
-                    {
-                        Temp.Add(cr[i]);
-                    }
+
+                        Temp.Add(cr[i++]);
+
+                        //Adding the last inverted comma
+                        if (cr[i] == '"') Temp.Add(cr[i]);
+
+                    } while (cr[i] != '"' && i < cr.Count() - 1 && cr[i] != '\n');//Break Conditions - \n(New Line), ", end of file
                     tokens.Add(Temp.Empty());
                 }
-                else if (cr[i] == '\'')
-                {
+                //FOR CHARACTERS
 
-                }
-                else
+                if (cr[i] == '\'') // checke if the character is '
                 {
-
                 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                //    Temp.Add(cr[i]);
+                //    i++;
+                //    while (cr[i] != '"' && i < cr.Count() - 1)
+                //    {
+                //        if (cr[i] == '\\') //Single Slash ('\')
+                //        {
+                //            Temp.Add(cr[i]);
+                //            i++;
+                //            Temp.Add(cr[i]);
+                //        }
+                //        else if (cr[i] == '\n')
+                //        {
+                //            tokens.Add(Temp.Empty());
+                //            break;
+                //        }
+                //        else 
+                //        {
+                //            Temp.Add(cr[i]);
+                //        }
+                //        i++;
+                //    }
+                //    if (cr[i] == '"' || cr[i] == '\\') 
+                //    {
+                //        Temp.Add(cr[i]);
+                //    }
+                //    tokens.Add(Temp.Empty());
+                //}
+                //else if (cr[i] == '\'')
+                //{
+
+                //}
+                //else
+                //{
+
+                //}
             }
             foreach (var item in tokens)
             {
